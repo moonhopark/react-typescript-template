053b05a0056eebfc9cefbdf07ddd693d4c4f8bd0
moonhopark <moonho4199@naver.com>
2023-02-09 15:49:36 +0900

    setting: style, assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import './styles.css';
+import IMAGE from './react.png';
+import HOME from './home.svg';
+
 const App = () => {
-  return <h1>React Typescript Webpack Template</h1>;
+  return (
+    <>
+      <h1>React Typescript Webpack Template</h1>
+      <img src={IMAGE} alt="React Logo" width="300" />
+      <img src={HOME} alt="Home SVg" width="300" />
+    </>
+  );
 };
 
 export default App;
