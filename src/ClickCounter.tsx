68e66ca2c1f53a193169e56dcab3ef34c30d489f
moonhopark <moonho4199@naver.com>
2023-02-22 14:37:01 +0900

    setting: eslint

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 
-type Props = {};
-
-const ClickCounter = (props: Props) => {
+const ClickCounter = () => {
   const [count, setCount] = useState(0);
 
   return (
