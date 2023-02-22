2c0c599ce797cca98f4487ce8b972b6ca386953d
moonhopark <moonho4199@naver.com>
2023-02-22 14:50:56 +0900

    setting: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import './styles.css';
-import IMAGE from './react.png';
-import HOME from './home.svg';
-import ClickCounter from './ClickCounter';
+import './styles.css'
+import IMAGE from './react.png'
+import HOME from './home.svg'
+import ClickCounter from './ClickCounter'
 
 const App = () => {
   return (
     <>
       <h1>
-        Edited Hello React Typescript Webpack Template - {process.env.NODE_ENV} {process.env.name}
+        Edited Hello React Typescript Webpack Template - {process.env.NODE_ENV}{' '}
+        {process.env.name}
       </h1>
       <img src={IMAGE} alt="React Logo" width="300" />
       <img src={HOME} alt="Home SVg" width="300" />
       <ClickCounter />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const ClickCounter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <div>
       <button onClick={() => setCount((c) => c + 1)}>Count {count}</button>
     </div>
-  );
-};
+  )
+}
 
-export default ClickCounter;
+export default ClickCounter
